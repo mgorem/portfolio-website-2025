b4364d577f3974948a1e7806a9be8ef36defb8ac
Orem M Gitonga <62249915+mgorem@users.noreply.github.com>
2025-12-10 11:08:33 +1100

    checking out to work on hero section

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,35 @@
  * @license Apache-2.0
  */
 
-import { Button } from "@/components/ui/button";
+/**
+ * Node Modules
+ */
+import { motion } from "motion/react"
+
+/**
+ * Custom Modules
+ */
+
+
+/**
+ * Components
+ */
+import { Hero } from "@/components/ui/Hero";
+
+/**
+ * Constants
+ */
+
 
 export const App = () => {
-  return <Button>Welcome to Orem's Portfolio!</Button>;
+  return (
+    <main className="flex-flex-col container mx-auto
+    p-10 max-w-4xl lg:pr-10
+     lg:pl-0 lg:max-w-6xl">
+
+      {/* App Content Goes Here */}
+      <Hero />
+      
+    </main>
+  );
 }
